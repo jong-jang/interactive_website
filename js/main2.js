826af08d467cee3559851bd2eaea9f3fb11ed371
jong-jang <jjh0299@gmail.com>
2023-12-05 21:18:53 +0900

    현재 활성화 영역

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,7 @@
 (() => {
 	let yOffset = 0; // window.scrollY 대신 쓸 변수
+	let prevScrollHeight = 0; //현재 스크롤 위치보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+	let currentScene = 0; // 현재 활성화된 (눈 앞에 보고있는) 씬(scroll-section)
 
 	const sceneInfo = [
 		{
@@ -48,7 +50,22 @@
 		}
 	}
 
-	function scrollLoop() {}
+	function scrollLoop() {
+		prevScrollHeight = 0;
+		for (let i = 0; i < currentScene; i++) {
+			prevScrollHeight += sceneInfo[i].scrollHeight;
+		}
+
+		if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+			currentScene++;
+		}
+		if (yOffset < prevScrollHeight) {
+			if (currentScene === 0) return; // 브라우저 바운스 효과로 인해 마이너스가 되는것을 방지(모바일)
+			currentScene--;
+		}
+
+		console.log(currentScene);
+	}
 
 	window.addEventListener('resize', setLayout);
 	window.addEventListener('scroll', () => {
