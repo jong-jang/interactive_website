b7716ee0a117b8435ea8a6ef16809342b0f6e394
jong-jang <jjh0299@gmail.com>
2023-12-20 21:02:53 +0900

    메뉴 일정 스크롤 오면 고정처리 / 백드롭 필터 적용

--- js/main2.js
+++ js/main2.js
@@ -141,6 +141,14 @@
 	}
 	setCanvasImages();
 
+	function checkMenu() {
+		if (yOffset > 44) {
+			document.body.classList.add('local-nav-sticky');
+		} else {
+			document.body.classList.remove('local-nav-sticky');
+		}
+	}
+
 	function setLayout() {
 		// 각 스크롤 섹션의 높이 세팅
 		for (let i = 0; i < sceneInfo.length; i++) {
@@ -438,6 +446,7 @@
 	window.addEventListener('scroll', () => {
 		yOffset = window.scrollY;
 		scrollLoop();
+		checkMenu();
 	});
 	window.addEventListener('load', () => {
 		setLayout();
