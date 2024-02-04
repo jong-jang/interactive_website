8e7121146e0ffa5cb1f5d294202b507beafd8611
jong-jang <jjh0299@gmail.com>
2024-02-05 00:12:10 +0900

    로딩처리

--- js/main.js
+++ js/main.js
@@ -618,8 +618,6 @@
 
 	window.addEventListener('DOMContentLoaded', () => {
 		setLayout(); // 중간에 새로고침 시, 콘텐츠 양에 따라 높이 계산에 오차가 발생하는 경우를 방지하기 위해 before-load 클래스 제거 전에도 확실하게 높이를 세팅하도록 한번 더 실행
-		document.body.classList.remove('before-load');
-		setLayout();
 
 		let imgElem;
 		for (let i = 0; i < sceneInfo[3].objs.imagesPath.length; i++) {
@@ -665,5 +663,10 @@
 				document.body.removeChild(e.currentTarget);
 			}
 		});
+	});
+
+	window.addEventListener('load', () => {
+		document.body.classList.remove('before-load');
+		setLayout();
 	});
 })();
